14784fc99d680308f0696154abd78e2ba5c58883
Sanghyeop Lee <geniusky24@outlook.com>
2024-09-08 14:32:22 +0900

    Title: 반복 출력하기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240908/반복 출력하기/repeated-output.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+void printString(int N);
+
+int main() {
+    int N;
+    scanf("%d", &N);
+    void printString(int N);
+    return 0;
+}
+
+void printString(int N) {
+    int i;
+    for (i=0; i<N; i++) {
+        printf("12345^&*()_");
+    }
+}
