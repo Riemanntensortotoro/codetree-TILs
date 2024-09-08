6d3b67ef5835e173934e4db1df23b1761d5895dd
Sanghyeop Lee <geniusky24@outlook.com>
2024-09-08 13:01:12 +0900

    Title: 별 찍는 것을 5번 반복하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240908/별 찍는 것을 5번 반복하기/repeat-shooting-the-stars-five-times.cpp
+++ 240908/별 찍는 것을 5번 반복하기/repeat-shooting-the-stars-five-times.cpp
@@ -2,12 +2,15 @@
 using namespace std;
 
 void Print5Stars() {
-    for(int i=0;i<5;i++) {
-        cout << "**********" << "\n";
+    for(int i=0; i<10; i++) {
+        cout << "*";
     }
+    cout << "\n";
 }
  
 int main() {
-    Print5Stars();
+    for (int j=0; j<5; j++) {
+        Print5Stars();
+    }
     return 0;
 }
