3a5e306e6d1e67303e704f1e0aa233497305c9a2
Sanghyeop Lee <geniusky24@outlook.com>
2024-09-08 12:47:12 +0900

    Title: 다양하게 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240908/다양하게 출력/print-in-variety.cpp
+++ 240908/다양하게 출력/print-in-variety.cpp
@@ -5,6 +5,6 @@ int main() {
     cout << "Total days in Year" << "\n";
     cout << 365 << "\n";
     cout << "Circumference rate" << "\n";
-    cout << 3.1415926535;
+    cout << "3.1415926535";
     return 0;
 }
