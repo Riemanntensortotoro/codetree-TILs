ce0d1b253f0fdaaabef39bf7a35cda692ba136c0
Sanghyeop Lee <geniusky24@outlook.com>
2024-09-08 14:44:41 +0900

    Title: 함수를 이용해 직사각형 만들기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240908/함수를 이용해 직사각형 만들기/create-a-rectangle-using-a-function.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int n, m;
+    scanf("%d %d", &n, &m);
+    int i, j;
+        for (i=0; i<n; i++){
+            for (j=0; j<m; j++){
+                printf("1");
+            }
+            printf("\n");
+        }
+    return 0;
+}
