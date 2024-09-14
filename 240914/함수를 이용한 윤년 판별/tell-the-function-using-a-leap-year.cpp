2e9877dcf29b7a53030743c77dd2d30f5aad497c
Sanghyeop Lee <geniusky24@outlook.com>
2024-09-14 16:19:58 +0900

    Title: 함수를 이용한 윤년 판별, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240914/함수를 이용한 윤년 판별/tell-the-function-using-a-leap-year.cpp
+++ 240914/함수를 이용한 윤년 판별/tell-the-function-using-a-leap-year.cpp
@@ -2,14 +2,7 @@
 using namespace std;
 
 bool yoonyear(int n) {
-    if (n % 4 == 0) {
-        if (n % 100) {
-            if (n % 400) return true;
-            return false;
-        }
-        return true;
-    }
-    return false;
+    return (n % 4 == 0 && (n % 100 != 0 || n % 400 == 0));
 }
 
 int main() {
