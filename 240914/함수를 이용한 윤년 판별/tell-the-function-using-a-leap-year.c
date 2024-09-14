363d98aee0f9fc9f847e8b0e1ab6e492a076390b
Sanghyeop Lee <geniusky24@outlook.com>
2024-09-14 16:26:34 +0900

    Title: 함수를 이용한 윤년 판별, Time: 2ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240914/함수를 이용한 윤년 판별/tell-the-function-using-a-leap-year.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+bool yoonyear(int n) {
+    if (n % 4 != 0) return false;
+    else if (n % 100 != 0) return true;
+    else if (n % 400 == 0) return false;
+    return true;
+}
+
+int main() {
+    int y;
+    scanf("%d", &y);
+
+    if (yoonyear(y)) {
+        printf("true");
+    }
+    else {
+        printf("false");
+    }
+    return 0;
+}
